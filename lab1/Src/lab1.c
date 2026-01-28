7f5e6993c367d4dc77f15530c1dafec359d7f4c7
ELLIOT KOHLES <u1179751@lab1-27.eng.utah.edu>
2026-01-28 09:50:49 -0700

    LED button implementation complete

--- lab1/Src/lab1.c
+++ lab1/Src/lab1.c
@@ -3,83 +3,106 @@
 
 void SystemClock_Config(void);
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Reset peripherals, init Flash + SysTick */
+    HAL_Init();
 
-  while (1)
-  {
- 
-  }
-  return -1;
+    /* Configure system clock */
+    SystemClock_Config();
+
+    // =====================================================
+    // CLOCK ENABLE
+    // =====================================================
+    // Enable GPIOC (LEDs) and GPIOA (USER button)
+    RCC->AHBENR |= (1 << 19); // GPIOCEN
+    RCC->AHBENR |= (1 << 17); // GPIOAEN
+
+    // =====================================================
+    // GPIOC: PC8 & PC9 OUTPUT (LEDs)
+    // =====================================================
+
+    // MODER: 01 = output
+    GPIOC->MODER &= ~((3 << (8 * 2)) | (3 << (9 * 2)));
+    GPIOC->MODER |=  ((1 << (8 * 2)) | (1 << (9 * 2)));
+
+    // OTYPER: push-pull
+    GPIOC->OTYPER &= ~((1 << 8) | (1 << 9));
+
+    // OSPEEDR: low speed
+    GPIOC->OSPEEDR &= ~((3 << (8 * 2)) | (3 << (9 * 2)));
+
+    // PUPDR: no pull-up/down
+    GPIOC->PUPDR &= ~((3 << (8 * 2)) | (3 << (9 * 2)));
+
+    // Initial LED state: PC8 ON, PC9 OFF
+    GPIOC->ODR |=  (1 << 8);
+    GPIOC->ODR &= ~(1 << 9);
+
+    // =====================================================
+    // GPIOA: PA0 INPUT (USER BUTTON)
+    // =====================================================
+
+    // MODER: 00 = input
+    GPIOA->MODER &= ~(3 << (0 * 2));
+
+    // OSPEEDR: low speed
+    GPIOA->OSPEEDR &= ~(3 << (0 * 2));
+
+    // PUPDR: 10 = pull-down
+    GPIOA->PUPDR &= ~(3 << (0 * 2));
+    GPIOA->PUPDR |=  (2 << (0 * 2));
+
+    // =====================================================
+    // MAIN LOOP
+    // =====================================================
+    while (1)
+    {
+        // If USER button pressed (PA0 high)
+        if (GPIOA->IDR & (1 << 0))
+        {
+            // Toggle LEDs
+            GPIOC->ODR ^= (1 << 8) | (1 << 9);
+            HAL_Delay(200); // simple debounce
+        }
+    }
 }
 
 /**
   * @brief System Clock Configuration
-  * @retval None
   */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK |
+                                  RCC_CLOCKTYPE_SYSCLK |
+                                  RCC_CLOCKTYPE_PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
-  /* User can add their own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+    __disable_irq();
+    while (1)
+    {
+    }
 }
-
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* User can add their own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-}
-#endif /* USE_FULL_ASSERT */
